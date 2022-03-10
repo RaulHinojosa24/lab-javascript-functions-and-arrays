37b9217a92f6c9a5e1f1d048b8a1bded5441dd3c
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-10 11:27:18 +0100

    Solucionados hasta el 7

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,66 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  return num2;
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length == 0) return null;
 
+  let longest = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+
+  return longest;
+}
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbersArray) {
+  let sum = 0;
+  for (let i = 0; i < numbersArray.length; i++) {
+    sum += numbersArray[i];
+  }
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(mixedArray) {
+  let sum = 0;
+
+  for (let i = 0; i < mixedArray.length; i++) {
+    switch (typeof (mixedArray[i])) {
+      case "string":
+        sum += mixedArray[i].length;
+        break;
+      case "boolean":
+        sum += +mixedArray[i];
+        break;
+      case "number":
+        sum += mixedArray[i];
+        break;
+      default:
+        throw new Error("Unsupported data type sir or ma'am")
+    }
+  }
+
+  return sum;
+}
 
 
 
@@ -26,16 +68,57 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbersArr) {
+  if (numbersArr.length == 0) return null;
+
+  let sum = 0;
+
+  for (let i = 0; i < numbersArr.length; i++) {
+    sum += numbersArr[i];
+  }
+
+  return sum / numbersArr.length;
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(wordsArr) {
+  if (wordsArr.length == 0) return null;
+
+  let sum = 0;
+
+  for (let i = 0; i < wordsArr.length; i++) {
+    sum += wordsArr[i].length;
+  }
+
+  return sum / wordsArr.length;
+}
 
 // Bonus - Iteration #4.1
-function avg() {}
+function avg(mixedArray) {
+  if (mixedArray.length == 0) return null;
+  let sum = 0;
+
+  for (let i = 0; i < mixedArray.length; i++) {
+    switch (typeof (mixedArray[i])) {
+      case "string":
+        sum += mixedArray[i].length;
+        break;
+      case "boolean":
+        sum += +mixedArray[i];
+        break;
+      case "number":
+        sum += mixedArray[i];
+        break;
+      default:
+        throw new Error("Unsupported data type sir or ma'am")
+    }
+  }
+
+  return Math.floor((sum / mixedArray.length) * 100) / 100;
+}
 
 // Iteration #5: Unique arrays
 const wordsUnique = [
@@ -52,14 +135,29 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(wordsUnique) {
+  if (wordsUnique.length == 0) return null;
 
+  let unique = [];
+
+  for (let i = 0; i < wordsUnique.length; i++) {
+    if (unique.indexOf(wordsUnique[i]) == -1) {
+      unique.push(wordsUnique[i]);
+    }
+  }
+
+  return unique;
+}
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(words, search) {
+  if (words.length == 0) return null;
+
+  return words.includes(search);
+}
 
 
 
@@ -78,7 +176,14 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
+function howManyTimes(words, search) {
+  let counter = 0;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] == search) counter++;
+  }
+
+  return counter;
+}
 
 
 
@@ -106,7 +211,7 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct() { }
 
 
 
